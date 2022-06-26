f9b6ca3c411c10b4fb959e1af86effae4d7dba8f
Hojinida <96263955+hojinida@users.noreply.github.com>
2022-06-26 23:09:03 +0900

    Time: 116 ms (25.60%), Space: 13.8 MB (58.67%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,8 +1,4 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         x=str(x)
-        n=len(x)
-        for i in range(0,n//2):
-            if x[i] != x[n-1-i]:
-                return False
-        return True
+        return True if "".join(reversed(list(x)))== x else False
