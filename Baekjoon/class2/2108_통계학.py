77803b04078487381e643e83b4403531dd4880e9
Hojinida <opqrs1103@gmail.com>
2022-12-28 03:43:39 +0900

    feat: 2108_통계학

--- Baekjoon/class2/2108_통계학.py
+++ Baekjoon/class2/2108_통계학.py
@@ -5,9 +5,10 @@ N = int(input())
 
 arr = sorted([int(input()) for _ in range(N)])
 cnt = Counter(arr)
-cnt = sorted(list(cnt.most_common()),key=lambda x: -x[1])
+cnt = sorted(list(cnt.most_common()), key=lambda x: -x[1])
 
-sys.stdout.write(str(round(sum(arr)/N)) + '\n')
+sys.stdout.write(str(round(sum(arr) / N)) + '\n')
 sys.stdout.write(str(arr[N // 2]) + '\n')
-sys.stdout.write(str(cnt[1][0]) + '\n') if len(cnt) > 1 and cnt[0][1] == cnt[1][1] else sys.stdout.write(str(cnt[0][0]) + '\n')
+sys.stdout.write(str(cnt[1][0]) + '\n') if len(cnt) > 1 and cnt[0][1] == cnt[1][1] else sys.stdout.write(
+    str(cnt[0][0]) + '\n')
 sys.stdout.write(str(arr[N - 1] - arr[0])) if N > 1 else sys.stdout.write(str('0'))
