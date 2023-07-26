34fc2343945cb78cc06f8a7ee8145e12382db48f
장호진 <8528231@naver.com>
2023-07-26 13:34:16 +0900

    10807_개수 세기

---
+++ BakingDog/Array/10807_개수 세기.py
@@ -0,0 +1,7 @@
+N = int(input())
+
+a = [int(i) for i in input().split()]
+
+v = int(input())
+
+print(a.count(v))
--- BakingDog/Array/1475_방번호.py
+++ BakingDog/Array/1475_방번호.py
@@ -1,4 +1,10 @@
-import sys
-from collections import Counter
+s = input()
 
+counter = [0] * 10
 
+for i in range(len(s)):
+    counter[int(s[i])] += 1
+counter[6] = (counter[6] + counter[9] + 1) // 2
+counter[9] = 0
+
+print(max(counter))
