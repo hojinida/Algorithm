17823a053df46791535967a7d4fad84aa14bfcfd
Hojin <8528231@naver.com>
2023-08-29 03:13:26 +0900

    1406_에디터

--- BakingDog/LinkedList/1406_에디터.py
+++ BakingDog/LinkedList/1406_에디터.py
@@ -1,6 +1,28 @@
-s = [i for i in input()]
+from collections import deque
+import sys
 
-cursor=len(s)
+input = sys.stdin.readline
 
-n = int(input())
+s = deque(input().strip())
+
+left_stack = deque()
+right_stack = s
+
+n = int(input().strip())
+
+for _ in range(n):
+    data = input().strip()
+    if data[0] == 'P':
+        right_stack.append(data[2])
+    elif data == 'L':
+        if right_stack:
+            left_stack.append(right_stack.pop())
+    elif data == 'D':
+        if left_stack:
+            right_stack.append(left_stack.pop())
+    elif data == 'B':
+        if right_stack:
+            right_stack.pop()
+
+print(''.join(right_stack) + ''.join(left_stack)[::-1])
 
