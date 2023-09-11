e9eb1a74972a954e5e3ac9da6f5d8fa0d4783611
Hojin <8528231@naver.com>
2023-09-11 13:16:29 +0900

    3015_오아시스 재결합

---
+++ BakingDog/stack/3015_오아시스 재결합.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+data = [int(input()) for _ in range(n)]
+
+stack = deque()
+
+answer=n-1
+for d in data:
+    while stack and stack[-1] <= d:
+        stack.pop()
+    stack.append(d)
+    answer += len(stack)-1
+
+print(answer)
+
+
