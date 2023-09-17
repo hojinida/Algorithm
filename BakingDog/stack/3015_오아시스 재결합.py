e562d70e758e0dcd0e4226dccfa30183019be8a5
Hojin <8528231@naver.com>
2023-09-18 03:18:55 +0900

    3015_오아시스 재결합

--- BakingDog/stack/3015_오아시스 재결합.py
+++ BakingDog/stack/3015_오아시스 재결합.py
@@ -9,12 +9,17 @@ data = [int(input()) for _ in range(n)]
 
 stack = deque()
 
-answer=n-1
+answer = 0
 for d in data:
-    while stack and stack[-1] <= d:
-        stack.pop()
-    stack.append(d)
-    answer += len(stack)-1
+    while stack and stack[-1][0] < d:
+        answer += stack.pop()[1] + 1
+    if stack and stack[-1][0] == d:
+        stack.append([d, stack[-1][1] + 1])
+    elif stack and stack[-1][0] > d:
+        stack.append([d, 1])
+    else:
+        stack.append([d, 0])
+for s, i in stack:
+    answer += i
+
 print(answer)
-
-
