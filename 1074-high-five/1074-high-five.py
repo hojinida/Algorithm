de2edbd991e1b0c5fae85d379537614fce342824
Hojinida <96263955+hojinida@users.noreply.github.com>
2024-06-27 03:46:38 +0900

    Time: 69 ms (65.01%), Space: 16.8 MB (71.65%) - LeetHub

--- 1074-high-five/1074-high-five.py
+++ 1074-high-five/1074-high-five.py
@@ -2,14 +2,16 @@ import heapq
 from collections import defaultdict
 class Solution:
     def highFive(self, items: List[List[int]]) -> List[List[int]]:
-        scores = defaultdict(list)
+        scores = {}
         
         # 각 학생의 점수를 최소 힙에 저장하여 상위 5개만 유지
         for student, score in items:
+            if student not in scores:
+                scores[student] = []
             heapq.heappush(scores[student], score)
             if len(scores[student]) > 5:
                 heapq.heappop(scores[student])
-        
+
         # 최종 평균 계산 및 결과 정렬
         result = []
         for student, score_heap in scores.items():
