d05e8857869f8402f084b81939c440a0ba8c38e5
Hojinida <96263955+hojinida@users.noreply.github.com>
2024-06-27 01:20:55 +0900

    Time: 346 ms (7.17%), Space: 18.5 MB (14.12%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        answer= 0
+        for i in s:
+            for j in g:
+                if i >= j:
+                    answer+=1
+                    g.remove(j)
+                    break
+        
+        return answer
