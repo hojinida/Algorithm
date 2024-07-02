19e66dde6ebd00bdbc480f3d72e0f3cda78045f0
Hojinida <96263955+hojinida@users.noreply.github.com>
2024-07-02 13:16:36 +0900

    Time: 39 ms (45.94%), Space: 16.2 MB (99.67%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -1,5 +1,4 @@
 from collections import deque
-from collections import OrderedDict
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,30 +7,26 @@ from collections import OrderedDict
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        result = {}
+        if not root:
+            return []
 
-        q = deque([(root,0)])
-        while q:
-            p = q.popleft()
-            if not p[0]:
-                break
-                
-            if p[1] not in result:
-                result[p[1]] = []
-            result[p[1]].append(p[0].val)
+        column_table = {}
+        queue = deque([(root, 0)])
+        
+        while queue:
+            node, col = queue.popleft()
+            if col not in column_table:
+                column_table[col] = []
+            column_table[col].append(node.val)
 
-            if p[0].left:
-                q.append((p[0].left,p[1]-1))
-            if p[0].right:
-                q.append((p[0].right,p[1]+1))
-        result = OrderedDict(sorted(result.items()))
-
-        answer = []
-        for key,value in result.items():
-            answer.append(value)
-
-        return answer
-
+            if node.left:
+                queue.append((node.left, col - 1))
+            if node.right:
+                queue.append((node.right, col + 1))
+        
+        sorted_columns = sorted(column_table.items())
+        
+        return [val for col, val in sorted_columns]
         
 
         
