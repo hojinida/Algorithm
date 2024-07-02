6ab1526c5edf99eda95269987ce333a916ec2d5b
Hojinida <96263955+hojinida@users.noreply.github.com>
2024-07-02 11:34:24 +0900

    Time: 42 ms (88.36%), Space: 16.6 MB (71.02%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,22 +1,17 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         pattern= {}
-        pattern2 = {}
 
         for i in nums1:
             if i not in pattern:
                 pattern[i] = 0
             pattern[i] +=1
 
-        for i in nums2:
-            if i not in pattern2:
-                pattern2[i] = 0
-            pattern2[i] +=1
-
+        
         answer = []
-        for i in pattern:
-            if i in pattern2:
-                for j in range(min(pattern[i],pattern2[i])):
-                    answer.append(i)
+        for i in nums2:
+            if i in pattern and pattern[i] > 0:
+                answer.append(i)
+                pattern[i] -=1
         
         return answer
