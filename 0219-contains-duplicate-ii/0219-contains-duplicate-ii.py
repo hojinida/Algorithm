bfa7194243e0322ce1bc8bc26141b5805cbb1448
Hojinida <96263955+hojinida@users.noreply.github.com>
2024-07-02 10:08:45 +0900

    Time: 456 ms (63.65%), Space: 30 MB (12.2%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -2,17 +2,7 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         pattern = {}
 
-        for i in range(len(nums)):
-            if nums[i] not in pattern:
-                pattern[nums[i]] = []
-            pattern[nums[i]].append(i)
-        
-
-        for key, value in pattern.items():
-            length = len(value)
-            if length > 1:
-                for i in range(length-1):
-                    if value[i+1] - value[i] <= k:
-                        return True
-        
-        return False
+        for i, num in enumerate(nums):
+            if num in pattern and i - pattern[num] <= k:
+                return True
+            pattern[num] = i
