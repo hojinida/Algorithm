6eca37a8999580db7f262a8210cffe937a6da3e5
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-18 02:28:25 +0900

    Time: 7 ms (54.45%), Space: 20.3 MB (67.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,7 @@
+from collections import Counter
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pattern = dict()
-     
-        for n in nums:
-            if n not in pattern:
-                pattern[n] = 0
-            pattern[n]+=1
-        
-        sortedItems = sorted(pattern.items(), key=lambda x: x[1], reverse=True)
-        answer = []
-        for i in range(k):
-            answer.append(sortedItems[i][0])
-        return answer
+        count = Counter(nums)
+        return [item for item, _ in heapq.nlargest(k, count.items(), key=lambda x: x[1])]
