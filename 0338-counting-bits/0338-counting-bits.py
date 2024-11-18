38cca5f05714f0db2c5bc2009a9cc3a15a52196d
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-18 16:53:21 +0900

    Time: 63 ms (13.09%), Space: 17.7 MB (56.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,24 +1,16 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        array = [2 ** i for i in range(17)]
-        pattern = [True for _ in range(17)]
+        def countBit(n):
+            result = 0
+            while n>0:
+                if n%2 == 1:
+                    result+=1
+                n //= 2
+            return result
+
         answer = []
-
-        value = 1
         for i in range(n+1):
-            if 2** value ==i:
-                value+=1
-            for j in range(1,value):
-                if array[j] == 0:
-                    array[j] = 2**j
-                    pattern[j] = not pattern[j]
-                array[j] -=1
-            pattern[0] = not pattern[0]
-            temp = 0
-            for j in range(value):
-                if pattern[j] : temp+=1
-            
-            answer.append(temp)
-
-           
+            answer.append(countBit(i))
         return answer
+
+            
