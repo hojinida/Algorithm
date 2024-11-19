f2ece5e56c75bc2f07f13e1f5bc89d28d1a5d276
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-20 02:06:24 +0900

    Time: 2 ms (98.6%), Space: 16.9 MB (14.67%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -8,11 +8,14 @@ class Solution:
                 return 
             
             for i in range(start, len(candidates)):
+                if candidates[i] > target:
+                    break
                 current.append(candidates[i])
                 back(current, i, target - candidates[i])
                 current.pop()
 
         result = []
+        candidates.sort()
         back([], 0, target)
         return result
                 
