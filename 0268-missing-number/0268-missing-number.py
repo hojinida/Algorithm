536662b13d785bc3901b4bc85a8c00ffe9843bff
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-21 03:40:12 +0900

    Time: 7 ms (39.73%), Space: 18.3 MB (8.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,10 @@
+from collections import Counter
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        length = len(nums)
-
         pattern = {i for i in nums}
 
-        for i in range(length+1):
+        for i in range(len(nums)+1):
             if i not in pattern:
                 return i
+        
+        
