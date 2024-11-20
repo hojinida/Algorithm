c60a3274eaff6ff3a2588cb4a5f01d264ae20572
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-21 02:52:47 +0900

    Time: 9 ms (46.96%), Space: 22.3 MB (8.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,5 @@
-import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = "".join([i.lower() for i in s if i.isalnum()])
-        print(s)
+    
         return s == s[::-1]
