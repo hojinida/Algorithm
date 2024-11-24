922e29082294645dafa846840f7fc6796816533a
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-24 22:29:37 +0900

    Time: 43 ms (7.3%), Space: 30.2 MB (7.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,10 @@
 from collections import Counter
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pattern = dict()
+        pattern = defaultdict(list)
 
         for s in strs:
             count = frozenset(Counter(s).items())
-            if count not in pattern:
-                pattern[count] = []
             pattern[count].append(s)
 
         return [i for i in pattern.values()]
