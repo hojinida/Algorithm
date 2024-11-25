23b5587b4c0b30e56294a3a8e3ff3f3cd16d8a8c
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-25 17:22:15 +0900

    Time: 0 ms (100%), Space: 16.8 MB (13.74%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -9,23 +9,22 @@ class Solution:
         answer = []
 
         while top <= bottom and left <= right:
-            for col in range(left,right+1):
+            for col in range(left, right + 1):
                 answer.append(matrix[top][col])
-            top+=1
+            top += 1
+
+            for row in range(top, bottom + 1):
+                answer.append(matrix[row][right])
+            right -= 1
 
             if top <= bottom:
-                for row in range(top,bottom+1):
-                    answer.append(matrix[row][right])
-                right-=1
-
-            if left <= right and top <= bottom:
-                for col in range(right,left-1,-1):
+                for col in range(right, left - 1, -1):
                     answer.append(matrix[bottom][col])
-                bottom-=1
+                bottom -= 1
 
-            if top <= bottom and left <= right:
-                for row in range(bottom,top-1,-1):
+            if left <= right:
+                for row in range(bottom, top - 1, -1):
                     answer.append(matrix[row][left])
-                left+=1
+                left += 1
 
         return answer
