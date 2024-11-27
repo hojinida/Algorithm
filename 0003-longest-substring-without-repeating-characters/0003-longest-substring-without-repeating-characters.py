f37c10e114cea78e84af689d5fb17266edde7397
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-28 02:23:36 +0900

    Time: 11 ms (95.21%), Space: 17.6 MB (11.15%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,22 +1,14 @@
-from collections import deque
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         maxLen = 0
-        q = deque()
-        pattern = set()
+        start = 0
+        pattern = {}
 
-        for i in range(len(s)):
-            if s[i] in pattern:
-                maxLen = max(maxLen,len(q))
-                while q:
-                    poped = q.popleft()
-                    pattern.remove(poped)
-                    if poped == s[i]:
-                        break
-   
-            q.append(s[i])
-            pattern.add(s[i])
-
-        maxLen = max(maxLen,len(q))
-
+        for end in range(len(s)):
+            if s[end] in pattern and pattern[s[end]] >= start:
+                start = pattern[s[end]] + 1
+            
+            pattern[s[end]] = end
+            maxLen = max(maxLen,end - start+1)
+        
         return maxLen
