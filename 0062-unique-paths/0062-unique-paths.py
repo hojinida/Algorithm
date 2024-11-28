f806c5a9c499746c6888288216c292a465ba1277
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-11-28 19:04:52 +0900

    Time: 0 ms (100%), Space: 17.5 MB (7.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,30 +1,14 @@
-from collections import deque
-
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        q = deque()
-        q.append((0, 0))
-        visited = set()
-        visited.add((0, 0)) 
-
         dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = 1 
 
-        while q:
-            x, y = q.popleft()
-
-            if x + 1 < m:
-                dp[x + 1][y] += dp[x][y]
-                if (x + 1, y) not in visited:
-                    q.append((x + 1, y))
-                    visited.add((x + 1, y))
-
-            if y + 1 < n:
-                dp[x][y + 1] += dp[x][y]
-                if (x, y + 1) not in visited:
-                    q.append((x, y + 1))
-                    visited.add((x, y + 1))
-
+        for i in range(m):
+            for j in range(n):
+                up = 0 if i-1 < 0 else dp[i-1][j]
+                left = 0 if j-1 < 0 else dp[i][j-1]
+                dp[i][j] += up+left
+                
         return dp[m - 1][n - 1]
 
 
