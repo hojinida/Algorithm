aaa638322eb92fb6531c8cd6afa84efde22045c2
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-12-01 02:05:05 +0900

    Time: 140 ms (18.32%), Space: 17.5 MB (6.82%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,15 +4,18 @@ class Solution:
         counts = Counter()
         l = 0
         max_len = 0
+        max_freq = 0  # 최빈 문자 개수를 저장
+
         for r in range(len(s)):
-            counts[s[r]]+=1
+            counts[s[r]] += 1
+            max_freq = max(max_freq, counts[s[r]])  # 갱신된 최빈 문자 개수 업데이트
 
-            max_freq = max(counts.values())
-
-            if (r - l +1) - max_freq > k:
-                counts[s[l]]-=1
-                l+=1
+            # 윈도우 크기가 조건을 초과하면 왼쪽 포인터 이동
+            if (r - l + 1) - max_freq > k:
+                counts[s[l]] -= 1
+                l += 1
             
+            # 최대 길이 갱신
             max_len = max(max_len, r - l + 1)
         
         return max_len
