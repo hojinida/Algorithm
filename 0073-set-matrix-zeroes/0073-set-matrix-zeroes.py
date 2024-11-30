9d5683249ef561c2c284a7bc41ebf7bb57a5acfe
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-12-01 00:45:54 +0900

    Time: 3 ms (70.87%), Space: 18 MB (6.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,33 +4,15 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         rows, cols = len(matrix), len(matrix[0])
-        first_row, first_col = False, False
+        zero_positions = []
 
-        # 첫 번째 행과 열에 0이 있는지 확인
         for i in range(rows):
-            if matrix[i][0] == 0:
-                first_col = True
-        for j in range(cols):
-            if matrix[0][j] == 0:
-                first_row = True
-
-        # 첫 번째 행과 열을 사용해 0 위치를 표시
-        for i in range(1, rows):
-            for j in range(1, cols):
-                if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    matrix[0][j] = 0
-
-        # 표시된 위치에 따라 행과 열을 0으로 설정
-        for i in range(1, rows):
-            for j in range(1, cols):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
-
-        # 첫 번째 행과 열 처리
-        if first_row:
             for j in range(cols):
-                matrix[0][j] = 0
-        if first_col:
-            for i in range(rows):
-                matrix[i][0] = 0
+                if matrix[i][j] == 0:
+                    zero_positions.append((i, j))
+
+        for x, y in zero_positions:
+            for col in range(cols):
+                matrix[x][col] = 0
+            for row in range(rows):
+                matrix[row][y] = 0
