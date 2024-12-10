e7465dcda6796f20172af55dce251263757df269
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-12-10 16:22:21 +0900

    Time: 0 ms (100%), Space: 17.3 MB (24.26%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.py
+++ 1720-crawler-log-folder/1720-crawler-log-folder.py
@@ -1,16 +1,9 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
         answer = 0
-
         for log in logs:
             if log == "../":
-                if answer != 0 :
-                    answer-=1
-            elif "./":
-                word = log.split("/")[0]
-                if word == ".":
-                    continue
-                else:
-                    answer+=1
-
+                answer = max(answer - 1, 0)
+            elif log != "./":
+                answer += 1
         return answer
