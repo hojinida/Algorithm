f7d79c956771a63a99df3e52a813dce16fe518ae
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-12-26 04:13:50 +0900

    Time: 0 ms (100%), Space: 18 MB (9.54%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,22 +1,14 @@
-from collections import OrderedDict
-
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pattern = OrderedDict([i,-1] for i in nums1)
+        stack = []
+        next_greater = {}
         
-        for i in range(len(nums2)-1):
-            for j in range(i+1,len(nums2)):
-                if nums2[i] < nums2[j]:
-                    if nums2[i] in pattern:
-                        pattern[nums2[i]] = nums2[j]
-                    break
+        for num in nums2:
+            while stack and stack[-1] < num:
+                next_greater[stack.pop()] = num
+            stack.append(num)
         
-        answer = []
-
-        for key,value in pattern.items():
-            answer.append(value)
-            
-        return answer
+        return [next_greater.get(num, -1) for num in nums1]
         
         
 
