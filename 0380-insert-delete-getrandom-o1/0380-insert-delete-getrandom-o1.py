0b4d84d0f639c7eccc1ee109a86d2c32d4edc67e
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2025-01-04 23:35:23 +0900

    Time: 109 ms (79.25%), Space: 58 MB (5.17%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -3,25 +3,31 @@ class RandomizedSet:
 
     def __init__(self):
         self.dic = {}
-        self.length = 0
+        self.arr = []
 
     def insert(self, val: int) -> bool:
         if val in self.dic:
             return False
-        self.dic[val] = 0
-        self.length+=1
+        self.dic[val] = len(self.arr) 
+        self.arr.append(val)    
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.dic:
             return False
-        del(self.dic[val])
-        self.length-=1
+        idx = self.dic[val]
+        last_val = self.arr[-1]
+
+        self.arr[idx] = last_val
+        self.dic[last_val] = idx
+
+        self.arr.pop()
+        del self.dic[val]
+
         return True
 
     def getRandom(self) -> int:
-        pick = random.randrange(0,self.length)
-        return list(self.dic.keys())[pick]
+        return random.choice(self.arr)
 
         
 
