08005c1fcc20443a8013016d8ae03c46b55256c1
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2025-01-11 23:03:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,22 +1,18 @@
+def count_ways(n):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+
+    for i in range(1, n + 1):
+        if i >= 1:
+            dp[i] += dp[i - 1]
+        if i >= 2:
+            dp[i] += dp[i - 2]
+        if i >= 3:
+            dp[i] += dp[i - 3]
+
+    return dp[n]
+
 count = int(input())
-
-def func(num):
-    global answer
-    global target
-
-    if num == target:
-        answer+=1
-        return
-
-    if num > target:
-        return
-
-    func(num+1)
-    func(num+2)
-    func(num+3)
-
 for _ in range(count):
-    answer = 0
     target = int(input())
-    func(0)
-    print(answer)
+    print(count_ways(target))
