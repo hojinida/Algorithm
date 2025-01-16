be25b8a653ff0ec735b2be8c4f206d94edfaeed0
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2025-01-16 22:43:08 +0900

    [Silver II] Title: 제곱수의 합, Time: 176 ms, Memory: 111112 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+dp = [float('inf')] * (n + 1)
+dp[0] = 0
+
+for i in range(1, n + 1):
+    j = 1
+    while j * j <= i:
+        dp[i] = min(dp[i], dp[i - j * j] + 1)
+        j += 1
+
+print(dp[n])
